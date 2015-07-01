db66705eee25ecf5633b60286d9db3a6ab26ed30
Mike Kaufmann <wulfland@hotmail.com>
2015-07-01 16:19:23 +0200

    Automatically create package in build.

--- src/SPOEmulators/Properties/AssemblyInfo.cs
+++ src/SPOEmulators/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SPOEmulators")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A package to emulate SharePoint CSOM.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Kaufmann")]
 [assembly: AssemblyProduct("SPOEmulators")]
 [assembly: AssemblyCopyright("Copyright ©  M. Kaufmann 2015")]
 [assembly: AssemblyTrademark("")]
